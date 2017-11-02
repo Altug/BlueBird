52e912b03704eb57bc574b931e1bb1ac74b9f79f
Altug Gurkaynak <10451030+altug@users.noreply.github.com>
2017-11-02 09:44:11 +0300

    Update twitterFetcher.js

--- _BlueBird.v.0.5-github/js/twitterFetcher.js
+++ _BlueBird.v.0.5-github/js/twitterFetcher.js
@@ -1,5 +1,3 @@
-/* MaviKus v.0.1 - icisleri bakanligi isay projesi icin gelistirilmistir. */
-
 (function(root, factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
